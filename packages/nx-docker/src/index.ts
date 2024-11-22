0e613213df35b62bfe96f79bc7869dfb02772997
John Martin <john.itvn@gmail.com>
2024-11-22 22:35:49 +0000

    fix(nx-docker): update publish  api

--- packages/nx-docker/src/index.ts
+++ packages/nx-docker/src/index.ts
@@ -1,3 +1,2 @@
 import dockerBuildExecutor from './executors/build/executor';
-import dockerAnalyzeExecutor from './executors/analyze/executor';
-export { dockerBuildExecutor, dockerAnalyzeExecutor };
+export { dockerBuildExecutor };
