4aa230bebc69fe15f2e59be42710f42b440b2912
John Martin <john.itvn@gmail.com>
2024-11-23 08:16:32 +0000

    test: update test case

--- tools/nx-internal/src/executors/publish-devcontainer-feature/executor.ts
+++ tools/nx-internal/src/executors/publish-devcontainer-feature/executor.ts
@@ -3,7 +3,10 @@ import { ProjectUtils } from '@ebizbase/nx-devkit';
 import { execFileSync } from 'child_process';
 import { PublishDevcontainerFeatureExecutorSchema } from './schema';
 
-const runExecutor: PromiseExecutor<PublishDevcontainerFeatureExecutorSchema> = async (_options, context) => {
+const runExecutor: PromiseExecutor<PublishDevcontainerFeatureExecutorSchema> = async (
+  _options,
+  context
+) => {
   let projectUtils;
   try {
     projectUtils = new ProjectUtils(context);
@@ -11,11 +14,25 @@ const runExecutor: PromiseExecutor<PublishDevcontainerFeatureExecutorSchema> = a
     logger.fatal('No project name provided', error);
     return { success: false };
   }
-  const commands = ['npx', 'devcontainer', 'features', 'publish', '--registry', 'ghcr.io', '--namespace', 'ebizbase/devcontainer-features', projectUtils.getProjectRoot() + '/src/' + projectUtils.getProjectName()];
+  const commands = [
+    'npx',
+    'devcontainer',
+    'features',
+    'publish',
+    '--registry',
+    'ghcr.io',
+    '--namespace',
+    'ebizbase/devcontainer-features',
+    projectUtils.getProjectRoot() + '/src/' + projectUtils.getProjectName(),
+  ];
 
-  execFileSync(commands[0], commands.slice(1), { cwd: context.root, stdio: 'inherit' });
-
-  return { success: true };
+  try {
+    execFileSync(commands[0], commands.slice(1), { cwd: context.root, stdio: 'inherit' });
+    return { success: true };
+  } catch (error) {
+    logger.fatal('Error while executing the publish command', error);
+    return { success: false };
+  }
 };
 
 export default runExecutor;
